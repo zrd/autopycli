5d264435d9ea9635fb9403ac1fad3b16bcf74e99
zrd <zdink23@gmail.com>
2018-10-03 02:46:36 -0700

    Stub out configurable & environment variables

--- sample_app.py
+++ sample_app.py
@@ -9,6 +9,8 @@ def main():
     app_config = CliRuntime(description="Some CLI app")
     app_config.arguments.add_argument("-s", "--sample", dest="sample_var", required=True, help="A required argument")
     app_config.arguments.add_argument("-o", "--option", dest="optional_var", help="An optional argument")
+    app_config.arguments.add_argument("-c", "--config", dest="config_var", help="A configurable option")
+    app_config.arguments.add_argument("-e", "--env", dest="env_var", help="An environment variable")
 
 
 if __name__ == "__main__":
