54c3bb86bcb6406ad532ebdbce78b5a3fa5f9657
zrd <zdink23@gmail.com>
2018-10-04 02:07:36 -0700

    Restructure CliRuntime & update sample_app

--- autopycli/cli_runtime.py
+++ autopycli/cli_runtime.py
@@ -1,8 +1,34 @@
-from autopycli import Arguments
+import autopycli.arguments as cliargs
+#from autopycli import ArgumentParser
+from autopycli import ArgumentsError
 from autopycli import Configuration
 from autopycli import Environment
 
 
+class RuntimeConfig:
+    def __repr__(self):
+        return "RuntimeConfig({})".format(str(self.__dict__))
+
+    def __str__(self):
+        members = ["{}: {}".format(k, str(self.__dict__[k])) for k in self.__dict__]
+        return "{{{}}}".format(", ".join(members))
+
+
 class CliRuntime:
     def __init__(self, *args, **kwargs):
-        self.arguments = Arguments(args, kwargs)
+        self.arg_parser = cliargs.ArgumentParser(*args, **kwargs)
+        self.runtime_config = RuntimeConfig()
+        self.required_args = []
+        self.error_messages = []
+
+    def add_argument(self, *args, **kwargs):
+        if kwargs.get("required"):
+            self.required_args.append(kwargs["dest"])
+
+        self.arg_parser.add_argument(*args, **kwargs)
+
+    def parse_args(self):
+        try:
+            self.arg_parser.parse_args(namespace=self.runtime_config)
+        except ArgumentsError as exc:
+            self.error_messages.append(str(exc))
--- sample_app.py
+++ sample_app.py
@@ -1,16 +1,24 @@
 import os
-
+import pprint
 from autopycli import CliRuntime
+from autopycli import logger
 
 CONFIG_PATH = os.path.join(os.getcwd(), "sample.conf")
-
+PP = pprint.PrettyPrinter()
 
 def main():
     app_config = CliRuntime(description="Some CLI app")
-    app_config.arguments.add_argument("-s", "--sample", dest="sample_var", required=True, help="A required argument")
-    app_config.arguments.add_argument("-o", "--option", dest="optional_var", help="An optional argument")
-    app_config.arguments.add_argument("-c", "--config", dest="config_var", help="A configurable option")
-    app_config.arguments.add_argument("-e", "--env", dest="env_var", help="An environment variable")
+    app_config.add_argument("-s", "--sample_var", dest="sample_var", required=True, help="A required argument")
+    app_config.add_argument("-o", "--optional_var", dest="optional_var", help="An optional argument")
+    app_config.add_argument("-c", "--config_var", dest="config_var", required=True, help="A configurable option")
+    app_config.add_argument("-e", "--env_var", dest="env_var", help="An environment variable")
+    app_config.add_argument("-d", "--default_var", dest="default_var", default="somedefault",
+                            help="An argument with a default")
+    app_config.parse_args()
+    PP.pprint(vars(app_config.arg_parser))
+    logger.debug(app_config.runtime_config)
+    logger.debug(app_config.required_args)
+    logger.debug(app_config.error_messages)
 
 
 if __name__ == "__main__":
