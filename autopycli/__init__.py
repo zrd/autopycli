58cf8516be14aa97d42495576062929cb80567ab
zrd <zdink23@gmail.com>
2018-10-04 02:05:52 -0700

    Subclass argparse.ArgumentParser

--- autopycli/__init__.py
+++ autopycli/__init__.py
@@ -1,4 +1,6 @@
-from autopycli.arguments import Arguments
+from autopycli.arguments import ArgumentParser
+from autopycli.arguments import ArgumentsError
 from autopycli.configuration import Configuration
 from autopycli.environment import Environment
 from autopycli.cli_runtime import CliRuntime
+from autopycli.logger import logger
--- autopycli/arguments.py
+++ autopycli/arguments.py
@@ -1,12 +1,10 @@
 import argparse
 
 
-class Arguments:
-    def __init__(self, *args, **kwargs):
-        self.arg_parser = argparse.ArgumentParser(args, kwargs)
+class ArgumentsError(Exception):
+    pass
 
-    def add_argument(self, *args, **kwargs):
-        self.arg_parser.add_argument(args, kwargs)
 
-    def parse_args(self):
-        return self.arg_parser.parse_args()
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise ArgumentsError(message)
