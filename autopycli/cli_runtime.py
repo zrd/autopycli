a21d76edccb74b945bdb9eb840d0772af61c5ae2
zrd <zdink23@gmail.com>
2018-10-06 03:45:15 -0700

    Stub out file & environment config

--- autopycli/cli_runtime.py
+++ autopycli/cli_runtime.py
@@ -1,8 +1,12 @@
+import inspect
+import os
+
 import autopycli.arguments as cliargs
 from autopycli import ArgumentsError
 from autopycli import Configuration
 from autopycli import Environment
-import inspect
+
+ARG_PARSER = cliargs.ArgumentParser
 
 
 class RuntimeConfig:
@@ -16,26 +20,38 @@ class RuntimeConfig:
 
 class CliRuntime:
     def __init__(self, *args, **kwargs):
-        parser_class = cliargs.ArgumentParser
-        parser_kwargs = self.filter_parser_args(parser_class, kwargs)
-        self.arg_parser = parser_class(*args, **parser_kwargs)
         self.runtime_config = RuntimeConfig()
         self.required_args = []
         self.error_messages = []
         self.config_path = kwargs.get("config_path")
+        self.env_vars = os.environ
+        parser_kwargs = self.filter_parser_args(ARG_PARSER, kwargs)
+        self.arg_parser = ARG_PARSER(*args, **parser_kwargs)
 
     def filter_parser_args(self, parser, kwargs):
+        """Return keyword args that are valid for the given parser.
+
+        Exclude comparisons with the 0th element in the argspec, as it
+        is the instance variable (i.e., "self").
+        """
         arg_spec = inspect.getargspec(parser.__init__)
         return {k: kwargs[k] for k in kwargs if k in arg_spec.args[1:]}
 
     def add_argument(self, *args, **kwargs):
         if kwargs.get("required"):
-            self.required_args.append(kwargs["dest"])
+            # TODO: Decide which is more correct
+            #self.required_args.append(kwargs["dest"])
+            self.required_args.append(kwargs.get("dest"))
 
         self.arg_parser.add_argument(*args, **kwargs)
+
+    def load_configs(self):
+        return
 
     def parse_args(self):
         try:
             self.arg_parser.parse_args(namespace=self.runtime_config)
         except ArgumentsError as exc:
             self.error_messages.append(str(exc))
+
+        self.load_configs()
